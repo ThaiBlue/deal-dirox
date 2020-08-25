b4bf62feb36c784b9b1ca0c5caa0eb3409258e30
Cao Tien <tien.tran@dirox.net>
2020-08-25 18:01:54 +0700

    UI - Dirox

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,28 +1,29 @@
 <template>
     <div class="menubar">
-        <div class="header">
-            <img src="../assets/menu-icon.png" width="52" >
-            <div class="options">
-                <div class="home">
-                    <img src="../assets/home-icon.png" width="52">
-                    <label>Home</label>
-                </div>
+        <div class="menu">
+            <div class="header">
+                <img src="../assets/menu-icon.png" width="52">
+                <div class="options">
+                    <div class="home">
+                        <img src="../assets/home-icon.png" width="52">
+                        <label>Home</label>
+                    </div>
 
-                <div class="sent">
-                    <img src="../assets/sent-icon.png" width="46">
-                    <label>Send Mail</label>    
-                </div>
+                    <div class="sent">
+                        <img src="../assets/sent-icon.png" width="46">
+                        <label>Send Mail</label>    
+                    </div>
 
-                <div class="user">
-                    <img src="../assets/user-icon.png" width="52">
-                    <label>Profile</label>
+                    <div class="user">
+                        <img src="../assets/user-icon.png" width="52">
+                        <label>Profile</label>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="setting">
-            <img src="../assets/setting-icon.png" width="56">
-            <label class="setting-label">Setting</label>
+            <div class="setting">
+                <img src="../assets/setting-icon.png" width="56">
+                <label class="setting-label">Settings</label>
+            </div>
         </div>
     </div>
 </template>
@@ -42,6 +43,16 @@ export default {
         height: 834px;
         display: flex;
         flex-direction: column;
+        justify-content: space-around;
+        /* align-items: center; */
+    }
+
+    .menu{
+        /* border: 1px solid black; */
+        height: 780px;
+        width: 201px;
+        display: flex;
+        flex-direction: column;
         justify-content: space-between;
     }
 
@@ -50,6 +61,8 @@ export default {
         justify-content: space-evenly;
         align-items: center;
         margin-bottom: 23px;
+        width: 201px;
+        height: 58px;
     }
 
     label {
@@ -74,10 +87,12 @@ export default {
     }
 
     .header {
-        height: 321px;
-        margin-top: 37px;
+        /* border: 1px solid black; */
+        height: 325px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
     }
+
+    
 </style>
