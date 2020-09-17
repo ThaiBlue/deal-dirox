cfc56292ea4bd43cd01d109f9bac8bad63c80537
tientc <tien.tran@dirox.net>
2020-09-17 18:46:47 +0700

    -- #48541: Create Pop up Setting - Done

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,8 @@
     <div class="container">
         <router-link to="/"></router-link>
         <router-link to="/deal"></router-link>
-        <router-link to="/dmeohieusaonokhongchayduoc"></router-link>
-        <!-- <router-link to="/popup"></router-link> -->
-<<<<<<< HEAD
-        <router-link to="/setting"></router-link>
     <router-view></router-view>
-  </div> 
-=======
-        <router-view></router-view>
     </div>
->>>>>>> 17cdfbf3befa2c484a06898ebc706f8276d153b8
 </template>
 <script>
     import axios from 'axios'
@@ -19,9 +11,6 @@
         name: 'app',
         components: {},
         computed: {
-            // loggedIn() {
-            //     return this.$store.getters.loggedIn
-            // }
         },
     }
 </script>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,5 @@
 import LoginScreen from "./Layout/LoginScreen"
 import DealPage from "./Layout/DealPage"
-// import popup from "./components/PopupSelectFolder"
-<<<<<<< HEAD
-import test from "./components/test"
-import setting from "./components/Settings"
-
-=======
-// import test from "./components/test"
->>>>>>> 17cdfbf3befa2c484a06898ebc706f8276d153b8
 
 const routes = [
     {
@@ -23,31 +15,6 @@ const routes = [
         //     requiresAuth: true
         // }
     },
-
-    // {
-        
-    //     path: '/popup',
-    //     name: 'pop-up',
-    //     component: popup
-    // },
-
-<<<<<<< HEAD
-    {
-        path: '/dmeohieusaonokhongchayduoc',
-        component: test
-    },
-
-
-    {
-        path: '/setting',
-        component: setting
-    }
-=======
-    // {
-    //     path: '/dmeohieusaonokhongchayduoc',
-    //     component: test
-    // }
->>>>>>> 17cdfbf3befa2c484a06898ebc706f8276d153b8
 ]
 
 
